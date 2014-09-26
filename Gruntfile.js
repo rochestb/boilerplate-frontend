337ddcc09c1b5acfabde4a11af09e389a43b1254
Benjamin Sterling <benjamin.sterling@kenzomedia.com>
2014-09-26 09:31:29 -0600

    Clean up file

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,15 @@ module.exports = function(grunt) {
   // Measures the time each task takes
   require('time-grunt')(grunt);
 
-  grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+  grunt.registerTask(
+    'serve',
+    'start the server and preview your app, --allow-remote for remote access',
+    function(target) {
+
     if (grunt.option('allow-remote')) {
       grunt.config.set('connect.options.hostname', '0.0.0.0');
     }
+
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
