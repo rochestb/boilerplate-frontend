a7ea26f8809a359d702fd3887a830f1149cfb3fe
Benjamin Sterling <benjamin.sterling@kenzomedia.com>
2014-10-15 09:41:31 -0600

    Add string replace

---
+++ build/config/string-replace.js
@@ -0,0 +1,26 @@
+/**
+ * build.config.concat
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  // Load task
+  grunt.loadNpmTasks('grunt-string-replace');
+
+  // Config
+  return {
+    dist: {
+      options: {
+        replacements: [
+          {
+            pattern: 'app.css',
+            replacement: 'app.min.css'
+          }
+        ]
+      },
+      files: {
+        'dist/index.html': 'dist/index.html'
+      }
+    }
+  };
+};
