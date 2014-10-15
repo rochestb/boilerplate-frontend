35049ab935973521c735186433517e4b33221688
Benjamin Sterling <benjamin.sterling@kenzomedia.com>
2014-10-15 09:41:03 -0600

    Add concurrent which has all the minifications

---
+++ build/config/concurrent.js
@@ -0,0 +1,21 @@
+/**
+ * build.config.concurrent
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  // Load task
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  // Config
+  return {
+    dev: [],
+
+    dist: [
+      'imagemin:dist',
+      'svgmin:dist',
+      'cssmin:dist',
+      'htmlmin:dist'
+    ]
+  };
+};
