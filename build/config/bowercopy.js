a5d49295928c158cf5c55ce2d60c9a3cf1bc7ed8
Benjamin Sterling <benjamin.sterling@kenzomedia.com>
2014-10-30 16:04:05 -0600

    Fix jscs

--- build/config/bowercopy.js
+++ build/config/bowercopy.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
     },
 
     // should not be needed, here as a placeholder.  The only scenario where we
-    // will need to add files here is if, during the requirejs build, it is 
+    // will need to add files here is if, during the requirejs build, it is
     // deleting files you need that is outside the main app.
     dist: {}
   };
