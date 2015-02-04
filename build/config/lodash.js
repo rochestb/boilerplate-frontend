4a71f035189dfc64d66f16d301fc7479f93c8d2d
Benjamin Sterling <benjamin.sterling@kenzomedia.com>
2015-02-04 16:36:38 -0700

    Add lodash task

---
+++ build/config/lodash.js
@@ -0,0 +1,29 @@
+/**
+ * build.config.lodash
+ */
+module.exports = function(grunt) {
+  'use strict';
+
+  var config = grunt.config.get('config');
+
+  // Load task
+  grunt.loadNpmTasks('grunt-lodash');
+
+  // Config
+  return {
+    dev: {
+      // output location
+      'dest': config.js + '/lodash.js',
+      'options': {
+        // modifiers for prepared builds
+        // modern, strict, compat
+        // 'modifier': 'strict',
+        category: [
+          'collection',
+          'array',
+          'function'
+        ]
+      }
+    }
+  };
+};
